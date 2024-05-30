c1e0fa7c0778fef2cb740edd5a2a481be3b9a16d
pouya <pouya.t.003@gmail.com>
2024-05-30 14:56:33 +0330

    added class Game

---
+++ Game.hpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+#include "Player.hpp"
+
+#ifndef GAME_HPP
+#define GAME_HPP
+
+class Game{
+    public:
+    // TODO: game constructor
+    private:
+    std::vector<Player> Players;
+};
+#endif
