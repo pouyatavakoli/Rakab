dcb65fa4e394d8ed6cf447a7f624f139856d14b6
pouya <pouya.t.003@gmail.com>
2024-05-30 17:13:01 +0330

    added class Matarsak as a subclass of PurpleCard class

---
+++ Matarsak.cpp
@@ -0,0 +1,1 @@
+#include "Matarsak.hpp"
---
+++ Matarsak.hpp
@@ -0,0 +1,10 @@
+#include "YellowCard.hpp"
+
+#ifndef MATARSAK_HPP
+#define MATARSAK_HPP
+class Matarsak : public YellowCard
+{
+public:
+private:
+};
+#endif
