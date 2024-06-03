0619968b57a174ff5734eb27e7baec43c390845d
pouya <pouya.t.003@gmail.com>
2024-06-03 20:13:15 +0330

    give 10 cards to each player

--- Game.cpp
+++ Game.cpp
@@ -10,8 +10,8 @@ void Game::setPlayersCount(int PlayerCountVal)
 
 void Game::run()
 {
-    Interface interface; // create interface for the game
-    setPlayersCount(interface.getPlayersCountFromUser());  // how many players are gonna play
+    Interface interface;                                  // create interface for the game
+    setPlayersCount(interface.getPlayersCountFromUser()); // how many players are gonna play
     players.resize(playerCount);
 
     // get players name , age , color
@@ -23,4 +23,20 @@ void Game::run()
         // TODO: get color
         i++;
     }
+
+    // give 10 cards to each player
+    // TODO: implement shuffle 
+    for (int i = 0; i < 10; i++)
+    {
+        for (Player &player : players)
+        {
+            if (!mainDeck.getCardsVector().empty())
+            {
+                Card card = mainDeck.getCardsVector().back(); // Get the top card from the main deck
+                mainDeck.removeCardFromDeck(card);            // Remove the card from the main deck
+                player.addCardToDeck(card);                   // Add the card to the player's deck
+            }
+        }
+    }
+
 }
