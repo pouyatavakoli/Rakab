a07b96bad67a9878ea5da81351d61d9af9c118e7
Pouya Omidi <pouyaomidi8413@gnail.com>
2024-07-18 12:33:19 +0330

    Neshans are added

--- graphics/rakab/mapwindow.cpp
+++ graphics/rakab/mapwindow.cpp
@@ -1,6 +1,13 @@
 #include "mapwindow.h"
 #include "ui_mapwindow.h"
 #include "mainmenu.h"
+#include <QPainter>
+#include <QResizeEvent>
+#include <QMimeData>
+#include <QDragEnterEvent>
+#include <QDropEvent>
+#include <QDrag>
+#include <QMouseEvent>
 
 mapwindow::mapwindow(QWidget *parent) :
     QWidget(parent),
@@ -8,13 +15,23 @@ mapwindow::mapwindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    setAutoFillBackground(true);
-    QPixmap pixmap(":/new/map/assets/Map.png");
-    QSize size = this->size();  // Get the size of the window
-    QPixmap scaledPixmap = pixmap.scaled(size, Qt::KeepAspectRatioByExpanding);  // Scale the pixmap to fit the window
-    QPalette palette;
-    palette.setBrush(this->backgroundRole(), QBrush(scaledPixmap));
-    this->setPalette(palette); // Set the palette of the widget
+    // Initialize the background pixmap
+    backgroundPixmap = QPixmap(":/new/map/assets/Map.png");
+
+    // Set the stylesheet for the widget to show the background image
+    this->setStyleSheet("background-image: url(:/new/map/assets/Map.png);"
+                        "background-position: center;"
+                        "background-repeat: no-repeat;"
+                        "background-size: cover;");
+
+    neshan["NeshaneSolh"] = ":/new/symbols/assets/symbols/Pe_Sym.png";
+    neshan["NeshaneJang"] = ":/new/symbols/assets/symbols/Wa_Sym.png";
+
+    // Initialize the neshan labels
+    initializeNeshanLabels();
+
+    // Enable drag and drop
+    setAcceptDrops(true);
 }
 
 mapwindow::~mapwindow()
@@ -22,10 +39,72 @@ mapwindow::~mapwindow()
     delete ui;
 }
 
+void mapwindow::initializeNeshanLabels()
+{
+    // Get the labels from the UI and set up event filters
+    QLabel *label1 = ui->NeshaneSolh; // Assuming label1 is the object name of the first label in the designer
+    QLabel *label2 = ui->NeshaneJang; // Assuming label2 is the object name of the second label in the designer
+
+    label1->setPixmap(QPixmap(neshan["NeshaneSolh"]));
+    label2->setPixmap(QPixmap(neshan["NeshaneJang"]));
+
+    label1->installEventFilter(this);
+    label2->installEventFilter(this);
+}
+
 void mapwindow::on_pushButton_clicked()
 {
     mainmenu *menu = new mainmenu();
-    menu ->show();
+    menu->show();
     this->close();
 }
 
+void mapwindow::paintEvent(QPaintEvent *event)
+{
+    QPainter painter(this);
+    painter.drawPixmap(rect(), backgroundPixmap.scaled(size(), Qt::IgnoreAspectRatio, Qt::SmoothTransformation));
+    QWidget::paintEvent(event); // Call the base class implementation
+}
+
+// Handle drag enter events
+void mapwindow::dragEnterEvent(QDragEnterEvent *event)
+{
+    event->acceptProposedAction();
+}
+
+// Handle drop events
+void mapwindow::dropEvent(QDropEvent *event)
+{
+    QLabel *label = qobject_cast<QLabel *>(childAt(event->position().toPoint()));
+    if (label && label->objectName().startsWith("Neshane")) {
+        label->move(event->position().toPoint() - label->rect().center());
+        label->show();
+    }
+}
+
+bool mapwindow::eventFilter(QObject *obj, QEvent *event)
+{
+    QLabel *label = qobject_cast<QLabel *>(obj);
+    if (label) {
+        if (event->type() == QEvent::MouseButtonPress) {
+            QMouseEvent *mouseEvent = static_cast<QMouseEvent *>(event);
+            if (mouseEvent->button() == Qt::LeftButton) {
+                QDrag *drag = new QDrag(this);
+                QMimeData *mimeData = new QMimeData;
+                mimeData->setText(label->objectName());
+                drag->setMimeData(mimeData);
+
+                // Set the pixmap and resize it to match the label size
+                QPixmap scaledPixmap = label->pixmap().scaled(label->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation);
+                drag->setPixmap(scaledPixmap);
+
+                // Set the hotSpot to be the cursor's position within the label
+                drag->setHotSpot(mouseEvent->pos());
+
+                drag->exec(Qt::MoveAction);
+                return true;
+            }
+        }
+    }
+    return QWidget::eventFilter(obj, event);
+}
--- graphics/rakab/mapwindow.h
+++ graphics/rakab/mapwindow.h
@@ -2,6 +2,10 @@
 #define MAPWINDOW_H
 
 #include <QWidget>
+#include <QMainWindow>
+#include <QPixmap>
+#include <QLabel>
+#include <QMap>
 
 namespace Ui {
 class mapwindow;
@@ -15,11 +19,20 @@ public:
     explicit mapwindow(QWidget *parent = nullptr);
     ~mapwindow();
 
+protected:
+    void paintEvent(QPaintEvent *event) override;
+    void dragEnterEvent(QDragEnterEvent *event) override;
+    void dropEvent(QDropEvent *event) override;
+    bool eventFilter(QObject *obj, QEvent *event) override;
+
 private slots:
     void on_pushButton_clicked();
 
 private:
     Ui::mapwindow *ui;
+    QPixmap backgroundPixmap;
+    QMap<QString, QString> neshan;
+    void initializeNeshanLabels();
 };
 
 #endif // MAPWINDOW_H
