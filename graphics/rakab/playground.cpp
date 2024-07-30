c8eb7ecd73e74cef1a03949927d734ba67216251
pouya <pouya.t.003@gmail.com>
2024-07-30 11:52:10 +0330

    fixed card click

--- graphics/rakab/playground.cpp
+++ graphics/rakab/playground.cpp
@@ -1,10 +1,29 @@
 #include "playground.h"
 
-Playground::Playground(Game& game,std::string province, QWidget *parent)
-    : QWidget(parent), game(game) {
-    setupUI();
+
+Playground::Playground(Game &game, const std::string province, QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::playground),game(game),
+    currentPlayerIndex(0)
+{
+    ui->setupUi(this);
+    //mainDeck  = game.getMainDeck();
+    game.fillMainDeck();
+    qDebug() << "filled deck";
+    game.shuffleDeck();
+    qDebug() << "shuffed deck" ;
+    game.handCardsToPLayers();
+    qDebug() << "handed cards to players ";
+    playerLayouts.append(ui->player1_hand);
+    playerLayouts.append(ui->player2_hand);
+    playerLayouts.append(ui->player3_hand);
+    playerLayouts.append(ui->player4_hand);
+    qDebug() << "added cards to layouts" ;
+    displayCards();
+    qDebug() << "called dispaly cards" ;
 }
 
+/*
 void Playground::setupUI() {
     playgroundLayout = new QGridLayout(this);
     tableLayout = new QVBoxLayout();
@@ -19,9 +38,12 @@ void Playground::setupUI() {
     setLayout(playgroundLayout);
     displayCards();
 }
+*/
 
 void Playground::displayCards() {
     // Clear existing cards from layouts
+    qDebug() << "display cards start";
+
     for (auto& layout : playerLayouts) {
         QLayoutItem* item;
         while ((item = layout->takeAt(0)) != nullptr) {
@@ -29,26 +51,28 @@ void Playground::displayCards() {
             delete item;
         }
     }
+    /*
     QLayoutItem* item;
     while ((item = tableLayout->takeAt(0)) != nullptr) {
         delete item->widget();
         delete item;
-    }
-
-    // Iterate through each player and display their cards
+    }*/
+    qDebug() << "deleted previous";
+    // Iterate through each player and display yellow and purple cards
     for (int i = 0; i < game.getPlayerCount(); ++i) {
-        for (const auto& card : game.getPlayerYellowHand(i)) {
-            Card* cardLabel = card.get();
-            cardLabel->setParent(this);
-            playerLayouts[i]->addWidget(cardLabel);
+        std::vector<Card*> player_hand; // Collecting player's cards here
 
-            connect(cardLabel, &Card::clicked, this, [this, cardLabel]() {
-                handleCardClick(cardLabel);
-            });
+        // Get yellow and purple cards and add them to player_hand
+        for (const auto& card : game.getPlayerYellowHand(i)) {
+            player_hand.push_back(card.get());
         }
 
         for (const auto& card : game.getPlayerPurpleHand(i)) {
-            Card* cardLabel = card.get();
+            player_hand.push_back(card.get());
+        }
+        qDebug() << "getters called";
+        // Now display all cards in player_hand
+        for (const auto& cardLabel : player_hand) {
             cardLabel->setParent(this);
             playerLayouts[i]->addWidget(cardLabel);
 
@@ -56,7 +80,8 @@ void Playground::displayCards() {
                 handleCardClick(cardLabel);
             });
         }
-
+        qDebug() << "displayed hand";
+        /*
         // Display cards on the table for each player
         for (const auto& card : game.getPlayerYellowOnTable(i)) {
             Card* cardLabel = card.get();
@@ -69,13 +94,17 @@ void Playground::displayCards() {
             cardLabel->setParent(this);
             tableLayout->addWidget(cardLabel);
         }
+        qDebug() << "displayed table";
+        */
     }
+    qDebug() << "display cards ended";
 }
+
 
 void Playground::handleCardClick(Card* card) {
     int playerIndex = game.getCurrentPlayerIndex();
     std::string cardName = card->getName();
-
+    qDebug() << "clicked on" << QString::fromStdString(cardName);
     // Tell the game to play the card
     game.playPlayerCard(playerIndex, cardName);
 
--- graphics/rakab/playground.h
+++ graphics/rakab/playground.h
@@ -7,21 +7,30 @@
 #include <QLabel>
 #include <memory>
 #include <vector>
+
+#include "ui_playground.h"
 #include "game.hpp"
 #include "card.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Playground; }
+QT_END_NAMESPACE
 
 class Playground : public QWidget {
     Q_OBJECT
 
 public:
-    explicit Playground(Game& game,std::string province, QWidget *parent = nullptr);
+    explicit Playground(Game& game, std::string province, QWidget *parent = nullptr);
     void setupUI();
 
 private:
+    Ui::playground *ui;
     Game& game;
     QGridLayout* playgroundLayout;
     QVBoxLayout* tableLayout;
-    QVector<QVBoxLayout*> playerLayouts;
+    QVector<QLayout*> playerLayouts;
+    int currentPlayerIndex;
+    //std::vector<std::shared_ptr<Card>> mainDeck;
 
     void displayCards();
     void updateUI();
